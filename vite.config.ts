0afb1d44ece02484a13b969af22b1c772d66d7ca
Arya Emami <aryaemami59@yahoo.com>
2024-03-12 21:50:10 -0500

    Add retry option to test configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     watch: false,
+    retry: 1,
     fileParallelism: !process.env.CI,
     coverage: {
       provider: 'v8',
